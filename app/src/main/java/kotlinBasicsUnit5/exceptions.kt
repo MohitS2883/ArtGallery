89232e22dc3c8fd0afa780a87a5d84905c84f481
MohitS2883 <svenkatmohit2883@gmail.com>
2025-01-27 12:17:29 +0530

    MED-4846 - Unit 5.1 Learnt about co routines how to run them how to define different co routines

---
+++ app/src/main/java/kotlinBasicsUnit5/exceptions.kt
@@ -0,0 +1,39 @@
+package kotlinBasicsUnit5import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+//        try {
+//            println(getWeatherReport())
+//        } catch (e: AssertionError) {
+//            println(e)
+//        }
+        println(getWeatherReport())
+        println("Have a good day!")
+    }
+}
+
+//suspend fun getWeatherReport() = coroutineScope {
+//    val forecast = async { getForecast() }
+//    val temperature = async {
+//        try {
+//            getTemperature()
+//        } catch (e: AssertionError) {
+//            println("Caught exception $e")
+//            "{ No temperature found }"
+//        }
+//
+//    }
+//    "${forecast.await()} ${temperature.await()}"
+//}
+//
+//suspend fun getForecast(): String {
+//    delay(1000)
+//    return "Sunny"
+//}
+//
+//suspend fun getTemperature(): String {
+//    delay(1000)
+//    throw AssertionError("Temperature is invalid")
+//    return "30\u00b0C"
+//}
